bf5ee703a43af4d97b32954eb7d1a904c35c4f90
tykowale <tykowale@gmail.com>
2015-04-18 20:25:52 -0500

    works for project euler

--- prime-factors/prime-finder.rb
+++ prime-factors/prime-finder.rb
@@ -1,12 +1,12 @@
-def factors(num, all_factors=[])
+def prime_factors(num, all_factors=[])
   2.upto(num) do |factor|
     if num % factor == 0
       all_factors << factor
       num = num / factor
-      return factors(num, all_factors)
+      return prime_factors(num, all_factors)
     end
   end
   all_factors
 end
 
-p factors(10)
+p prime_factors(600851475143)
